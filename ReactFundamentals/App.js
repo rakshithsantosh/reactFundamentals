d62415e82e62acfc6a3086804f32f1fad78af16f
rakshith_s <rakshithsantosh@gmail.com>
2025-02-08 07:47:00 +0530

    JSX

--- ReactFundamentals/App.js
+++ ReactFundamentals/App.js
@@ -1,10 +1,38 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("h1", { id: "something" }, [
-  React.createElement("p", { id: "child" }, "this is a header"),
-]);
+//React.createElement => Object => HTMLElement (render)
 
-const root = createRoot(document.getElementById("root"));
+const heading = React.createElement("h1", { id: "heading" }, "Hi there");
 
-root.render(parent);
+//JSX - HTML like syntax
+const headingOne = <h1>this is a heading</h1>;
+//JSX is tranpiled before this code reaches JS Engine parcel(bundler) transpiles this using babel package
+
+//Babel tranpiles JSX to JS
+
+//JSX => React.createElement => ReactElement JS Object => HTMLElement
+
+//React Component
+//Class Based Component
+//Functional Component
+
+//React Functional Component
+
+const Heading = () => {
+  return (
+    <>
+      <h1>Heading</h1>
+      {headingOne}
+      <Title />
+    </>
+  );
+};
+
+//JSX prevents cross site scripting even if we right some code in {} that is sanitized by JSX
+
+const Title = () => <div>this is some div</div>;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Heading />);
