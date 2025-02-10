8c2203610ae71c58727888b94df27e5ef6418ec3
rakshith_s <rakshithsantosh@gmail.com>
2025-02-10 08:25:44 +0530

    Project setUp

---
+++ foodApp/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const AppLayout = () => {
+  return <div className="app">this is a testcase</div>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
